077785e02284728304deb5c66c0280a708d99127
NISHA PATIL <prashantpatil@prashants-air.kpn>
2023-12-06 14:12:03 +0100

    added readme, technical requirement and schema to job model

---
+++ models/job.model.js
@@ -0,0 +1,21 @@
+const { Schema, model } = require('mongoose');
+
+const jobSchema = new Schema(
+    {
+        title: {
+            type: String,
+            required: true
+        },
+        company: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        salary: { type: Number }
+    }
+)
+
+module.exports = model("Job", jobSchema);
