94901b64bf4b6387deddd685ac93e6c3fb4693d3
Huseyin Hepyalniz <huseyinhepyalniz@huseyins-air.speedport.ip>
2023-12-06 15:48:53 +0100

    CRUD functionaliy added to job route

--- routes/job.routes.js
+++ routes/job.routes.js
@@ -4,6 +4,7 @@ const router = require ('express').Router();
 const Job = require('../models/job.model');
 
 
+// Create a new job
 router.post('/jobs', (req, res, next) => {
     const {title, company, description, salary} = req.body;
    
@@ -12,6 +13,46 @@ router.post('/jobs', (req, res, next) => {
       .catch(err => res.json(err));
   });
 
+// Read all jobs
+
+router.get('/jobs', (req, res, next) => {
+    Job.find()
+
+      .then(jobs => res.json(jobs))
+      .catch(err => res.json(err));
+  });
+
+
+// Read a specific job by ID
+
+router.get('/jobs/:id', (req, res, next) => {
+    const jobId = req.params.id;
+
+    Job.findById(jobId)
+        .then(job => res.json(job))
+        .catch(err => res.json(err));
+});
+
+// Update a specific job by ID
+
+router.put('/jobs/:id', (req, res, next) => {
+    const jobId = req.params.id;
+
+    Job.findByIdAndUpdate (jobId, req.body, {new: true})
+        .then(updatedJob => res.json(updatedJob))
+        .catch(err => res.json(err));
+});
+
+// Delete a specific job by ID
+
+
+router.delete('/jobs/:id', (req, res, next) => {
+    const jobId = req.params.id;
+
+    Job.findByIdAndDelete (jobId)
+        .then(()=> res.json({message: `Job with ${jobId} is removed succesfully`} ))
+        .catch(err => res.json(err));
+});
 
   module.exports = router;
   
